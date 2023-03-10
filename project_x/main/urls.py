d4dbe498ce7183257c12309b995c84010ce0fa46
AmeAya <ameameameaya@gmail.com>
2023-03-11 02:51:39 +0600

    static url

--- project_x/main/urls.py
+++ project_x/main/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from .yasg import urlpatterns as doc_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 api_patterns = [
@@ -10,6 +12,6 @@ api_patterns = [
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(api_patterns)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += doc_urls
