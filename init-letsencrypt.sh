69f219c3d58f0a957caada8507d9b39bbedb99a7
AmeAya <ameameameaya@gmail.com>
2023-03-20 15:15:49 +0600

    qwe

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(api.umit-dev.kz www.api.umit-dev.kz)
+domains=(api.umit-dev.kz)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
