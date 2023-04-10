229b3a18e52efe5c45513a77f2c3ef24c45de39f
AmeAya <ameameameaya@gmail.com>
2023-04-10 17:36:53 +0600

    fix

--- project_x/project/smtplib/mail.py
+++ project_x/project/smtplib/mail.py
@@ -1,18 +1,15 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-import os
 
 email_sender = 'arystanbek.abdrakhmanov@gmail.com'
 password = 'kfkoogzwebjdvisk'
 
-
-smtp_server = smtplib.SMTP('smtp.gmail.com', 587)
-
 def send_mail(mail, code):
     try:
-        smtp_server.login(email_sender, password)
+        smtp_server = smtplib.SMTP('smtp.gmail.com', 587)
         smtp_server.starttls()
+        smtp_server.login(email_sender, password)
         msg = MIMEMultipart()
         msg.attach(MIMEText(f'Ваш код: {code}'))
         email_getter = mail
