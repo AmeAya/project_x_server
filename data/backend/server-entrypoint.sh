9ff871ab4c542f5b342f83cd27b7fb2250cf3241
AmeAya <ameameameaya@gmail.com>
2023-04-10 09:31:14 +0600

    update

--- data/backend/server-entrypoint.sh
+++ data/backend/server-entrypoint.sh
@@ -19,10 +19,5 @@ python manage.py collectstatic --noinput
 
 gunicorn main.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
 
-until python manage.py loaddata dump.json
-do
-    echo 'Trying to loaddata'
-done
-
 # for debug
 python manage.py runserver 0.0.0.0:8000
